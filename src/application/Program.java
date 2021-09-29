7ae4d644c1c1159582dd93d4532e1a75c0312a7d
NatanZZ <natanmulazzani@gmail.com>
2021-09-29 10:55:33 -0300

    chess-system

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,41 @@
 package application;
 
+import java.util.Scanner;
+
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the row: ");
+        int row = sc.nextInt();
+        System.out.print("Enter the column: ");
+        int column = sc.nextInt();
+
+        Position position = new Position(row, column);
+
+        System.out.println(position);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        sc.close();
+        
         
     }
     
